665c5178245275dd036977dae621c662cbf9ab34
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-10-05 19:26:17 -0300

    feat: rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: ''
-  }
+    element: <Layout/>,
+    children: [
+      { index: true, element: <Home/> },
+      { path: '/projetos', element: <Projects/> },
+      { path: '/contato', element: <Contact/> }
+    ]
+  },
+  { path: '*', element: <NotFound/> }
 ]);
 
 function Routes() { return <RouterProvider router={router}/>; }
