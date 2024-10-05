98ecad7b2b93a85aea5655a26a1daf3ca6c497f5
Gustavo Atanazio <gus.atanazio@gmail.com>
2024-10-05 19:27:54 -0300

    chore: projetos e skills

---
+++ src/content.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    name: 'Studier',
+    description: 'Aprimore seus estudos por meio de quizzes! Web app construído com React, Typescript e Tailwind CSS.',
+    stack: ['React', 'TypeScript', 'Tailwind'],
+    sourceCode: 'https://github.com/gustavo-atanazio/Studier',
+    livePreview: 'https://studier-theta.vercel.app/'
+  },
+  {
+    name: 'Booker',
+    description: 'Projeto para gerenciamento de coleção de livros pessoais, construído com React, TypeScript e SASS.',
+    stack: ['React', 'TypeScript', 'SASS'],
+    sourceCode: 'https://github.com/gustavo-atanazio/Booker',
+    livePreview: 'https://booker-six.vercel.app/'
+  },
+  {
+    name: 'Weather',
+    description: 'Projeto de visualização do clima feito com React e TypeScript, consumindo a API da OpenWeather e da Unsplash.',
+    stack: ['React', 'TypeScript', 'CSS'],
+    sourceCode: 'https://github.com/gustavo-atanazio/Weather',
+    livePreview: 'https://weather-delta-flame.vercel.app/'
+  },
+  {
+    name: 'FE Hub',
+    description: 'A central de notícias e eventos da Formula E. Interaja, compartilhe, participe. O E-Prix continua aqui.',
+    stack: ['React', 'TypeScript', 'Tailwind'],
+    sourceCode: 'https://github.com/gustavo-atanazio/fe-hub',
+    livePreview: 'https://fe-hub.vercel.app/'
+  },
+  {
+    name: 'Snake game',
+    description: 'Jogo feito com HTML, CSS e JavaScript',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    sourceCode: 'https://github.com/gustavo-atanazio/snake-game',
+    livePreview: 'https://snake-game-cyan-one.vercel.app/'
+  },
+  {
+    name: 'Barbershop',
+    description: 'Site comercial para barbearias feito com React',
+    stack: ['React', 'JavaScript', 'CSS'],
+    sourceCode: 'https://github.com/gustavo-atanazio/Barbershop',
+    livePreview: 'https://barbershop-green.vercel.app/'
+  }
+];
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'SASS',
+  'Tailwind',
+  'Git'
+];
+
+export { projects, skills };
